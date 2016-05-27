37684c99e8fd56050caf12941f228fcf314e8627
Andrew Russo <russspruce@gmail.com>
2016-05-27 12:13:24 -0700

    Made more intergration to the backend, and making commit to save current progress.

--- js/github-interface.js
+++ js/github-interface.js
@@ -2,11 +2,11 @@ var apiKey = "9d29f9c14006a3698d3d4d117f357dda8920cf09";
 var getRepos = require('./../js/github.js').getRepos;
 
 $(document).ready(function() {
-  $('#githubSearch').click(function() {
+  var githubInfo = new getRepos();
+  $('#showGithub').click(function() {
     var username = $('#username').val();
     $('#username').val("");
-    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-      $('.showGithub').text("" + username +" currently has: " + response.public_repos +" public repositories.");
-    });
+    githubInfo.getAllRepos(username, displayFunction);
+
   });
 });
--- js/github.js
+++ js/github.js
@@ -1,10 +1,12 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(){
+}
 
   exports.getRepos.prototype.getAllRepos = function(username) {
     $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-      $('.showGithub').text("" + username +" currently has: " + response.public_repos +" public repositories.");
+      displayFunction(username, response.public_repos );
+    }).fail(function(error) {
+  $('.showGithub').text(error.responseJSON.message);
     });
-};
-};
+ };
